0b58e6936a3a5f8aa2b1cfeef8b09ccdfaf2ced6
Bryan Moore <bmoore@incadencecorp.com>
2016-08-05 13:51:48 -0400

    Modified start script to take input file as parameter

--- resources/bin/gdelt.sh
+++ resources/bin/gdelt.sh
@@ -2,5 +2,5 @@
 
 GDELT_HOME=/opt/gdelt_ingest
 
-java -cp "$GDELT_HOME/lib/*" com.incadencecorp.coalesce.ingester.gdelt.GDELT_Ingester $GDELT_HOME/conf/gdelt.properties $GDELT_HOME/data/20160722150000.export.CSV
+java -cp "$GDELT_HOME/lib/*" com.incadencecorp.coalesce.ingester.gdelt.GDELT_Ingester $GDELT_HOME/conf/gdelt.properties $*
 
